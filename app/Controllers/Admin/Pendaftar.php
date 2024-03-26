ed3b3412c2426064cf9f27054057f59066d9a3d4
Java Web Media <javawebmedia@gmail.com>
2024-03-26 15:18:26 +0700

    Update pendaftar

--- app/Controllers/Admin/Pendaftar.php
+++ app/Controllers/Admin/Pendaftar.php
@@ -3,6 +3,9 @@ namespace App\Controllers\Admin;
 
 use CodeIgniter\Controller;
 use App\Models\Jenis_dokumen_model;
+use App\Models\Dokumen_model;
+use App\Models\Agama_model;
+use App\Models\Akun_model;
 
 class Pendaftar extends BaseController
 {
