c73d1644f5a72bf923c43b1286234ff5c34ee561
Java Web Media <javawebmedia@gmail.com>
2024-05-18 11:33:00 +0700

    Update akun pendaftar

--- app/Controllers/Admin/Akun_pendaftar.php
+++ app/Controllers/Admin/Akun_pendaftar.php
@@ -11,8 +11,36 @@ class Akun_pendaftar extends BaseController
 {
 	public function index()
 	{
-		$data = [   'title'     => 'Data Akun Pendaftar',
-					'content'	=> 'admin/akun_pendaftar/index'
+		$m_akun 		= new Akun_model();
+		$pager 			= service('pager'); 
+		// akun
+		if(isset($_GET['keywords'])) 
+		{
+			$keywords 		= $this->request->getVar('keywords');
+			$total 			= $m_akun->total_cari($keywords);
+			$title 			= 'Hasil pencarian: '.$_GET['keywords'].' - '.$total.' ditemukan';
+	        $page    		= (int) ($this->request->getGet('page') ?? 1);
+	        $perPage 		= $this->website->paginasi();
+	        $total   		= $total;
+	        $pager_links 	= $pager->makeLinks($page, $perPage, $total,'bootstrap_pagination');
+	        $page 			= ($this->request->getGet('page'))?($this->request->getGet('page')-1)*$perPage:0;
+	        $akun 			= $m_akun->paginasi_admin_cari($keywords,$perPage, $page);
+		}else{
+			$total 			= $m_akun->total();
+			$title 			= 'Data Akun Pendaftar ('.$total.')';
+	        $page    		= (int) ($this->request->getGet('page') ?? 1);
+	        $perPage 		= $this->website->paginasi();
+	        $total   		= $total;
+	        $pager_links 	= $pager->makeLinks($page, $perPage, $total,'bootstrap_pagination');
+	        $page 			= ($this->request->getGet('page'))?($this->request->getGet('page')-1)*$perPage:0;
+	        $akun 			= $m_akun->paginasi_admin($perPage, $page);
+		}
+		// end akun
+
+		$data = [   'title'     	=> $title,
+					'akun'			=> $akun,
+					'pagination'	=> $pager_links,
+					'content'		=> 'admin/akun_pendaftar/index'
                 ];
         return view('admin/layout/wrapper',$data);
 	}
---
+++ app/Views/admin/akun_pendaftar/index.php
@@ -0,0 +1,113 @@
+<div class="row">
+	<div class="col-md-6">
+		<?php echo form_open(base_url('admin/akun_pendaftar'), ' method="get"') ?>
+		<div class="input-group">
+          <input type="text" name="keywords" class="form-control" placeholder="Keywords..." value="<?php if(isset($_GET['keywords'])) { echo $_GET['keywords']; } ?>" required>
+          <span class="input-group-append">
+            <button type="submit" name="submit" value="Cari" class="btn btn-info btn-flat">
+            	<i class="fa fa-search"></i> Cari
+            </button>
+            <a href="<?php echo base_url('admin/akun_pendaftar/tambah') ?>" class="btn btn-success">
+				<i class="fa fa-plus"></i> Tambah Baru
+			</a>
+          </span>
+        </div>
+        <?php echo form_close() ?>
+	</div>
+	<div class="col-md-6">
+			<?php if(isset($pagination)) { echo str_replace('index.php/','',$pagination); } ?>
+	</div>
+</div>
+<hr>
+
+<?php echo form_open(base_url('admin/akun_pendaftar/proses')) ?>
+<input type="hidden" name="pengalihan" value="<?php echo str_replace('index.php/','',CURRENT_URL()) ?>">
+<div class="mailbox-controls">
+<div class="input-group">
+	<button type="submit" name="submit" value="Delete" class="btn btn-secondary" title="Hapus Berita">
+		<i class="fa fa-trash"></i>
+	</button>
+	<button type="submit" name="submit" value="Draft" class="btn btn-dark" title="Jangan Publikasikan">
+		<i class="fa fa-eye-slash"></i>
+	</button>
+	<button type="submit" name="submit" value="Publish" class="btn btn-info" title="Publikasikan">
+		<i class="fa fa-eye"></i>
+	</button>
+
+	<select name="status_akun" class="form-control">
+		<option value="Aktif">Aktif</option>
+		<option value="Menunggu">Menunggu</option>
+		<option value="Non-Aktif">Non-Aktif</option>
+	</select>
+
+	<span class="input-group-append">
+		<button type="submit" name="submit" value="Update" class="btn btn-warning">
+			<i class="fa fa-search"></i> Update
+		</button>
+	</span>
+</div>
+
+
+
+<div class="table-responsive mailbox-messages mt-2">		
+
+<table class="table table-sm table-hover" id="example1">
+	<thead>
+		<tr class="text-left bg-light">
+			<th width="5%" class="text-center">
+				<button type="button" class="btn btn-default btn-sm checkbox-toggle">
+					<i class="far fa-square"></i>
+        		</button>
+			</th>
+			<th width="15%">Nama</th>
+			<th width="20%">Email</th>
+			<th width="15%">Jenis</th>
+			<th width="8%">NIS</th>
+			<th width="12%">NISN</th>
+			<th width="15%">Status</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php $no=1; foreach($akun as $akun) { ?>
+		<tr>
+			<td class="text-center">
+				<div class="icheck-primary">
+					<input type="checkbox" name="id_akun[]" value="<?php echo $akun->id_akun ?>" id="check_<?php echo $no ?>">
+					<label for="check_<?php echo $no ?>"></label>
+				</div>
+				<?php echo $no ?>
+			</td>
+			<td><?php echo $akun->nama ?></td>
+			<td><?php echo $akun->email ?></td>
+			<td><?php echo $akun->jenis_akun ?></td>
+			<td><?php echo $akun->nis ?></td>
+			<td><?php echo $akun->nisn ?></td>
+			<td class="text-left">
+				
+				<?php if($akun->status_akun=='Aktif') { ?>
+					<span class="badge bg-success">
+						<i class="fa fa-check-circle"></i> <?php echo $akun->status_akun ?>
+					</span>
+				<?php }elseif($akun->status_akun=='Menunggu') { ?>
+						<span class="badge bg-warning">
+							<i class="fa fa-clock"></i> <?php echo $akun->status_akun ?>
+						</span>
+				<?php }else{ ?>
+					<span class="badge bg-secondary">
+						<i class="fa fa-times-circle"></i> <?php echo $akun->status_akun ?>
+					</span>
+				<?php } ?>
+				
+			</td>
+			<td>
+				
+				<a href="<?php echo base_url('admin/akun_pendaftar/edit/'.$akun->id_akun) ?>" class="btn btn-success btn-sm"><i class="fa fa-edit"></i></a>
+				<a href="<?php echo base_url('admin/akun_pendaftar/delete/'.$akun->id_akun) ?>" class="btn btn-dark btn-sm" onclick="confirmation(event)"><i class="fa fa-trash"></i></a>
+			</td>
+		</tr>
+		<?php $no++; } ?>
+	</tbody>
+</table>
+</div>
+</div>
